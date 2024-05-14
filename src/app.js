95319d379e19e6a8571c2101d7bf2ffe77a1809e
Mardiyaaah <162523906+Mardiyaaah@users.noreply.github.com>
2024-05-14 16:00:27 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -111,12 +111,12 @@ document.addEventListener('alpine:init', () => {
 
     // format pesan whatsapp
     const formatMessage = (obj) => {
-        return `Data Customer:
+        return `*Data Customer* :
 - Nama: ${obj.name}
 - Alamat: ${obj.alamat}
 - No HP: ${obj.phone}
-> Data Pesanan:
- ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`)}
+*Data Pesanan* :
+> ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`)}
  Sub Total: ${rupiah(obj.total)}  
  Terimakasih. `;
     }
